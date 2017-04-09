f113e371dc1629308deaa393734e65fe2648f8be
john <john@osdev.localdomain>
2017-04-09 14:29:06 -0700

    adding tty functions

--- kernel.c
+++ kernel.c
@@ -1,24 +1,14 @@
-static char* const VGA_MEMORY = (char*)0xb8000;
-static const int VGA_WIDTH = 80;
-static const int VGA_HEIGHT = 25;
+#include "kernel/tty.h"
 
 void kernel_early(void)
 {
-    //early work
+    terminal_initialize();
 }
 
+__attribute__((noreturn))
 int main(void)
 {
-    const char *str = "Hello OS!";
-    unsigned int i=0; //place holder for the text string position
-    unsigned int j=0; //place holder for video buffer position
-
-    while(str[i] != '\0')
-    {
-        VGA_MEMORY[j] = str[i];
-        VGA_MEMORY[j+1] = 0x0a;
-        i++;
-        j=j+2;
-    }
+    printf("Hello OS!");
+    while(1) { }
     return 0;
 }
